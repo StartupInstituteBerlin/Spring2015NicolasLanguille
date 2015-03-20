b9006dd716595e8d1db8bd109dabf8484d7a2d06
nic-lan <narghy82@yahoo.it>
2015-03-20 15:49:14 +0100

    rating model : belongs to restourants and users + validates

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,7 @@
-class Rating < ActiveRecord::Base  
+class Rating < ActiveRecord::Base
+  belongs_to :users
+  belongs_to :restourants
+
+  validates :user_id, presence: true
+  validates :restourant_id, presence: true 
 end
