0c06362a00cdc4dd3789e372bbacbdcecff2b1c7
nic-lan <narghy82@yahoo.it>
2015-03-20 15:49:56 +0100

    rating model > validates on value

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,6 @@ class Rating < ActiveRecord::Base
   belongs_to :restourants
 
   validates :user_id, presence: true
-  validates :restourant_id, presence: true 
+  validates :restourant_id, presence: true
+  validates :value, presence: true
 end
