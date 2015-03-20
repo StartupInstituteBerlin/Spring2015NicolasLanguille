37671dcb00402c859b454fb71ca8c438f5838ca4
nic-lan <narghy82@yahoo.it>
2015-03-20 13:17:53 +0100

    added roles table and user_roles table and seeded them to assign existing users to registered and owner roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
   has_many :restourants
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  before_create :set_default_role
+
+  private
+  def set_default_role
+     @user_role = self.user_roles.new
+     @user_role.role_id = Role.find_by_name('registered')
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316122322) do
+ActiveRecord::Schema.define(version: 20150320110308) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "last_name"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20150316122322) do
     t.integer  "user_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "simple_captcha_data", force: :cascade do |t|
     t.string   "key",        limit: 40
     t.string   "value",      limit: 6
@@ -41,6 +47,13 @@ ActiveRecord::Schema.define(version: 20150316122322) do
   end
 
   add_index "simple_captcha_data", ["key"], name: "idx_key"
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
