a721fd87e5839db3dba35f7682f74c465871ad43
nic-lan <narghy82@yahoo.it>
2015-03-30 10:08:59 +0200

    db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category_names = [
+    'italian',
+    'french',
+    'german',
+    'arabic',
+    'chinese'
+    ]
+
+category_names.each do |category_name|
+
+  category = Category.create(
+              name: category_name
+              )
+
+  owner = User.create(
+            email: Faker::Internet.email,
+            password: "12345678",
+            password_confirmation: "12345678",
+            role_name: "owner"
+            )
+
+  registered_user = User.create(
+                      email: Faker::Internet.email,
+                      password: "12345678",
+                      password_confirmation: "12345678",
+                      role_name: "registered"
+                      )
+
+  10.times.each do
+    address = Faker::Address.street_name + ", " +
+              Faker::Address.zip + ", " +
+              Faker::Address.city
+
+    restourant = Restourant.create(
+                  name: Faker::Company.name,
+                  description: Faker::Lorem.sentence,
+                  phone: Faker::PhoneNumber.phone_number,
+                  address: address,
+                  user_id: owner.id,
+                  category_id: category.id
+                  )
+
+    5.times.each do
+      Reservation.create(
+        last_name: Faker::Name.last_name,
+        datetime: Faker::Time.forward(23,:evening),
+        no_people: Faker::Number.number(1),
+        email: Faker::Internet.email,
+        restourant_id: restourant.id
+        )
+    end
+  end
+end
+
+Restourant.all.each do |restourant|
+  User.where(role_name: "registered").all.each do |user|
+    Rating.create(
+      value: Faker::Number::between(1,5),
+      user_id: user.id,
+      restourant_id: restourant.id
+      )
+  end
+end
