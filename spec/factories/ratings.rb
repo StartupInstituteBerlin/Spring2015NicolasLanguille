41f93cb9e74b5ae29b9819c3189a97f439b5a5a9
nic-lan <narghy82@yahoo.it>
2015-03-31 11:32:52 +0200

    Add factory rating

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -1,3 +1,8 @@
 FactoryGirl.define do 
+  factory :rating do
+    value Faker::Number.between(1,5)
 
+    association :user_id, factory: :user
+    association :restourant_id, factory: :restourant
+  end
 end
