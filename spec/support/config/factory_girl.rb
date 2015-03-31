099795f86340a622cd3dac4e90075661818891a6
nic-lan <narghy82@yahoo.it>
2015-03-31 14:32:34 +0200

    Remove FactoryGirl.lint

--- spec/support/config/factory_girl.rb
+++ spec/support/config/factory_girl.rb
@@ -4,10 +4,8 @@ RSpec.configure do |config|
   config.before(:suite) do
     begin
       DatabaseCleaner.start
-      FactoryGirl.lint
     ensure
       DatabaseCleaner.clean
     end
   end
-  
 end
