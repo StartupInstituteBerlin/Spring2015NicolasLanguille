134f937c320fd3e9da78f414edf3d5387f0aca11
nic-lan <narghy82@yahoo.it>
2015-03-31 15:07:01 +0200

    Refactor valid factory on category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
 
-  it 'should has a valid Factory' do
-    create(:category).should be_valid
+  it { expect(create :category ).to be_valid }
+
   end
 end
