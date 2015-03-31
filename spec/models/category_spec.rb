3865b8101e6326e868ddf70c5eb4b0c38942352c
nic-lan <narghy82@yahoo.it>
2015-03-31 15:07:52 +0200

    add spec context attribute name capitalized

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Category, type: :model do
 
   it { expect(create :category ).to be_valid }
 
+  context "attributes" do
+    it "name is capitalized" do
+      category = create :category, name: "itaLian"
+      expect(category.name).to eq "Italian"
+    end
   end
 end
