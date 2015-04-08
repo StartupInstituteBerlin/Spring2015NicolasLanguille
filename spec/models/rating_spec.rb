362640517427dd0392830e668c0d59f9e1497c47
nic-lan <narghy82@yahoo.it>
2015-04-08 16:41:10 +0200

    Add rating Tests

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,4 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
+  let!(:registered_user) { create :user, role_name: "registered" }
+  let!(:owner) { create :user, role_name: "owner" }
+  let!(:restourant) { create :restourant, user_id: owner }
+
+  it "belongs_to a registered user and a restourant" do
+    rate = build :rating, restourant_id: restourant.id, user_id: registered_user.id
+
+    expect( rate ).to be_valid
+  end
+
+  it "does not belong to an owner user" do
+    rate = build :rating, user_id: owner.id, restourant_id: restourant.id
+    expect( rate ).not_to be_valid
+  end
+
+  it "validates presence of :user_id" do
+    rate = build :rating, user_id: nil, restourant_id: restourant.id
+    expect( rate ).not_to be_valid
+  end
+
+  it "validates presence of :restourant_id" do
+    rate = build :rating, user_id: registered_user.id, restourant_id: nil
+    expect( rate ).not_to be_valid
+  end
+
+  it "validates presence of :value" do
+    rate = build( :rating, user_id: registered_user.id,
+                           restourant_id: restourant.id,
+                           value: nil)
+    expect( rate ).not_to be_valid
+  end
+
 end
