f6bbc02407f7044addca740ab69107a2f92a5c68
nic-lan <narghy82@yahoo.it>
2015-04-08 16:40:54 +0200

    Adding Test for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
 
-  it { expect(create :category ).to be_valid }
-
-  context "attributes" do
-    it "name is capitalized" do
+  context 'attributes' do
+    it 'name is well formatted' do
       category = create :category, name: "itaLian"
-      expect(category.name).to eq "Italian"
+      expect( category.name ).to eq "Italian"
     end
+  end
+
+  context 'about validations' do
+    it 'is invalid without a name' do
+      category = build :category, name: nil
+      expect( category ).not_to be_valid
+    end
+
+    it 'has a unique name' do
+      create :category, name: "Italian"
+      category = build :category, name: "Italian"
+      expect( category ).not_to be_valid
+    end
+
+    # include_examples :is_unique, Category, :param, :string
   end
 end
