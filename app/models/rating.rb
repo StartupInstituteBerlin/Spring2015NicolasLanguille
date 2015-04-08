4486170af30e2cff26654cc979ea3881496bcb7f
nic-lan <narghy82@yahoo.it>
2015-04-08 16:34:18 +0200

    Add owner? helper for rating !!

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,6 +6,8 @@ class Rating < ActiveRecord::Base
   validates :restourant_id, presence: true
   validates :value, presence: true
 
+  before_validation :owner?
+
   def evaluate_restourant_rate
     restourant_ratings = Rating.where(restourant_id: self.restourant_id)
 
@@ -19,4 +21,13 @@ class Rating < ActiveRecord::Base
     #puts "\n\n\naverage: #{average}"
     #puts "\n\n\nrest value: #{restourant.rate}\n\n\n"
   end
+
+  private
+
+    def owner?
+      owner = User.where(id: self.user_id, role_name: "owner").exists?
+
+      self.errors.add("user_id","cannot be an owner") if owner
+    end
+
 end
